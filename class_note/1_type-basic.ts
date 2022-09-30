bd9b4c6a5c5828ca9da387e845d5bf5ebe6b2419
DevSeulgi <dev.dinobox@gmail.com>
2022-09-30 22:43:46 +0900

    first commit

---
+++ class_note/1_type-basic.ts
@@ -0,0 +1,27 @@
+// JS 문자열 선언 방법
+// const str = 'hello';
+
+// TS 문자열 선언 방법
+const str: string = 'hello';
+
+// TS 숫자
+const num: number = 10;
+
+// TS 배열
+const arr: Array<number> = [1,2,3];
+const arrStr: Array<string> = ['1','2','3'];
+const items: number[] = [1,2,3];
+const itemStr: string[] = ['1', '2'];
+
+// TS 튜플
+const animalAge: [string, number] = ['cat', 10];
+
+// TS 객체
+const obj: object = {};
+const person: { name: string, age: number } = {
+  name : 'seulgi',
+  age : 10
+};
+
+// TS 진위값
+const fact: boolean = true;
---
+++ why-ts/sample.ts
@@ -0,0 +1,8 @@
+function sum(a: number, b: number): number {
+  return a + b;
+};
+
+sum(10, 20);
+sum(1, '2');
+
+let result = sum(10,20);
